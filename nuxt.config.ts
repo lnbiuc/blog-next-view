26df34dc95f6ac40488802454a583bd956dfb040
violet <hi@lnbiuc.com>
2023-11-19 19:06:22 +0800

    Remove vite build configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,16 +21,6 @@ export default defineNuxtConfig({
 
   },
 
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: '[hash].[ext]',
-        },
-      },
-    },
-  },
-
   experimental: {
     // when using generate, payload js assets included in sw precache manifest
     // but missing on offline, disabling extraction it until fixed
