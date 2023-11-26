98ecbf94cfdaba33fe0074be51f334b3a3534366
lnbiuc <hi@lnbiuc.com>
2023-11-26 22:21:13 +0800

    Fix bug in login functionality

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
-  <div text="xl gray4" m-5 flex="~ gap3" justify-center>
-    <!-- <NuxtLink i-carbon-campsite to="/" /> -->
-    <!-- <a i-carbon-logo-github href="https://github.com/lnbiuc/blog-next-view" target="_blank" /> -->
+  <div text="xs gray4" m-5 p-5 flex="~ gap3" justify-center>
+    <!-- <NuxtLink i-carbon-campsite to="/" />
+    <a i-carbon-logo-github href="https://github.com/lnbiuc/blog-next-view" target="_blank" /> -->
+    <span>Copyright © 2023</span>
+    <span>Violet</span>
+    <a href="https://beian.miit.gov.cn/" target="_blank">陕ICP备2021015553号-1</a>
   </div>
 </template>
