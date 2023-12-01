992c248fb88560a86206c9f4c02d937d9ac4766f
ayanamirei <violetzzs@proton.me>
2023-12-01 23:38:03 +0800

    fix

--- pages/article/[shortLink].vue
+++ pages/article/[shortLink].vue
@@ -9,7 +9,7 @@ import MyGiscus from '~/components/Giscus/MyGiscus.vue'
 import { formatTime } from '~/composables/formatTime'
 
 interface RouteParams {
-  shortLink: string
+    shortLink: string
 }
 
 const route = useRoute()
@@ -22,10 +22,10 @@ const id = 'preview-only'
 const color = useColorMode()
 
 function getArticle() {
-  if (!shortLink)
-    return
-  getArticleByShortLink(shortLink).then((res) => {
-    article.value = res.data.value?.data as ArticleWithContent
+    if (!shortLink) return
+    getArticleByShortLink(shortLink).then((res) => {
+        article.value = res.data.value?.data as ArticleWithContent
+    })
 }
 getArticle()
 
@@ -34,95 +34,118 @@ const theme = ref<'light' | 'dark'>()
 let scrollElement: string | HTMLElement | undefined
 
 watchEffect(() => {
-  theme.value = color.value === 'light' ? 'light' : 'dark'
+    theme.value = color.value === 'light' ? 'light' : 'dark'
 })
 
 onMounted(() => {
-  scrollElement = document.documentElement
-  const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
-  theme.value = mediaQueryList.matches ? 'dark' : 'light'
+    scrollElement = document.documentElement
+    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
+    theme.value = mediaQueryList.matches ? 'dark' : 'light'
 })
 
-const mdHeadingId = (_text: any, _level: any, index: number) => `heading-${index}`
+const mdHeadingId = (_text: any, _level: any, index: number) =>
+    `heading-${index}`
 </script>
 
 <template>
-  <div>
-    <Head>
-      <Meta name="keywords" :content="article?.tags?.join(',') || 'Violet, Blog, Vue, Nuxt, TypeScript, JavaScript, Node.js, Web, Frontend, Backend, Fullstack, Developer, Programmer, Engineer, Software, Software Engineer, Software Developer, Software Programmer, Software Engineer, Software Developer'" />
-      <Meta property="og:title" :content="article?.title || 'Violet\'s Blog'" />
-      <Meta property="og:description" :content="article?.description || 'A blog for sharing knowledge test'" />
-      <Meta property="og:image" :content="article?.cover[0] || 'https://vio.vin/favicon.ico'" />
-      <Meta name="twitter:card" content="summary_large_image" />
-      <Meta name="twitter:creator" content="@lnbiuc" />
-      <Meta name="twitter:title" :content="article?.title || 'Violet\'s Blog'" />
-      <Meta name="twitter:description" :content="article?.description || 'A blog for sharing knowledge test'" />
-      <Meta name="twitter:image" :content="article?.cover[0] || 'https://vio.vin/favicon.ico'" />
-    </Head>
-    <NuxtLayout name="default">
-      <NuxtLayout name="home">
-        <div class="flex flex-col text-left">
-          <img v-if="article?.cover[0]" :src="article?.cover[0]" alt="cover" class="aspect-[2.5/1] w-full rounded-lg object-cover">
-          <div class="my-6 text-4xl font-bold">
-            {{ article?.title }}
-          </div>
-          <div class="mb-1">
-            {{ article?.description }}
-          </div>
-          <div class="mt-4 flex flex-row items-center justify-start text-violet">
-            <div class="i-carbon-view mr-2" />
-            <div>{{ article?.views }} views</div>
-            <div class="i-carbon-alarm mx-2 scale-110" />
-            <div>{{ formatTime(article?.updatedAt) }}</div>
-          </div>
-          <UDivider class="my-6" />
-        </div>
-        <div class="text-left lg:grid lg:grid-cols-[auto,250px] lg:gap-8">
-          <MdPreview :theme="theme" :md-heading-id="mdHeadingId" class="preview" :editor-id="id" :model-value="article?.content" :show-code-row-number="true" preview-theme="github" />
-          <div class="catalog relative">
-            <ClientOnly>
-              <MdCatalog :md-heading-id="mdHeadingId" :editor-id="id" :scroll-element-offset-top="20" :scroll-element="scrollElement" class="max-h-[100vh]" />
-            </ClientOnly>
-          </div>
-        </div>
-        <MyGiscus
-          class="mt-4 py-4"
-          repo="lnbiuc/blog-next-view"
-          repo-id="R_kgDOKsLYcQ"
-          category="Announcements"
-          category-id="DIC_kwDOKsLYcc4CbAW9"
-          mapping="pathname"
-          term="Welcome to @giscus/vue component!"
-          strict="1"
-          reactions-enabled="1"
-          emit-metadata="0"
-          input-position="top"
-          :theme="color.preference"
-          lang="en"
-          crossorigin="anonymous"
-        />
-      </NuxtLayout>
-    </NuxtLayout>
-  </div>
+    <div>
+
+        <Head>
+            <Meta name="keywords"
+                  :content="article?.tags?.join(',') || 'Violet, Blog, Vue, Nuxt, TypeScript, JavaScript, Node.js, Web, Frontend, Backend, Fullstack, Developer, Programmer, Engineer, Software, Software Engineer, Software Developer, Software Programmer, Software Engineer, Software Developer'" />
+            <Meta property="og:title"
+                  :content="article?.title || 'Violet\'s Blog'" />
+            <Meta property="og:description"
+                  :content="article?.description || 'A blog for sharing knowledge test'" />
+            <Meta property="og:image"
+                  :content="article?.cover[0] || 'https://vio.vin/favicon.ico'" />
+            <Meta name="twitter:card"
+                  content="summary_large_image" />
+            <Meta name="twitter:creator"
+                  content="@lnbiuc" />
+            <Meta name="twitter:title"
+                  :content="article?.title || 'Violet\'s Blog'" />
+            <Meta name="twitter:description"
+                  :content="article?.description || 'A blog for sharing knowledge test'" />
+            <Meta name="twitter:image"
+                  :content="article?.cover[0] || 'https://vio.vin/favicon.ico'" />
+        </Head>
+        <NuxtLayout name="default">
+            <NuxtLayout name="home">
+                <div class="flex flex-col text-left">
+                    <img v-if="article?.cover[0]"
+                         :src="article?.cover[0]"
+                         alt="cover"
+                         class="aspect-[2.5/1] w-full rounded-lg object-cover">
+                    <div class="my-6 text-4xl font-bold">
+                        {{ article?.title }}
+                    </div>
+                    <div class="mb-1">
+                        {{ article?.description }}
+                    </div>
+                    <div class="mt-4 flex flex-row items-center justify-start text-violet">
+                        <div class="i-carbon-view mr-2" />
+                        <div>{{ article?.views }} views</div>
+                        <div class="i-carbon-alarm mx-2 scale-110" />
+                        <div>{{ formatTime(article?.updatedAt) }}</div>
+                    </div>
+                    <UDivider class="my-6" />
+                </div>
+                <div class="text-left lg:grid lg:grid-cols-[auto,250px] lg:gap-8">
+                    <MdPreview :theme="theme"
+                               :md-heading-id="mdHeadingId"
+                               class="preview"
+                               :editor-id="id"
+                               :model-value="article?.content"
+                               :show-code-row-number="true"
+                               preview-theme="github" />
+                    <div class="catalog relative">
+                        <ClientOnly>
+                            <MdCatalog :md-heading-id="mdHeadingId"
+                                       :editor-id="id"
+                                       :scroll-element-offset-top="20"
+                                       :scroll-element="scrollElement"
+                                       class="max-h-[100vh]" />
+                        </ClientOnly>
+                    </div>
+                </div>
+                <MyGiscus class="mt-4 py-4"
+                          repo="lnbiuc/blog-next-view"
+                          repo-id="R_kgDOKsLYcQ"
+                          category="Announcements"
+                          category-id="DIC_kwDOKsLYcc4CbAW9"
+                          mapping="pathname"
+                          term="Welcome to @giscus/vue component!"
+                          strict="1"
+                          reactions-enabled="1"
+                          emit-metadata="0"
+                          input-position="top"
+                          :theme="color.preference"
+                          lang="en"
+                          crossorigin="anonymous" />
+            </NuxtLayout>
+        </NuxtLayout>
+    </div>
 </template>
 
 <style scoped>
 .catalog {
-  position: sticky;
-  top: 10px;
-  overflow: auto;
-  height: auto;
-  max-height: calc(100vh - 50px);
+    position: sticky;
+    top: 10px;
+    overflow: auto;
+    height: auto;
+    max-height: calc(100vh - 50px);
 }
-.md-editor-dark, .md-editor {
-  --md-bk-color: transparent !important;
+.md-editor-dark,
+.md-editor {
+    --md-bk-color: transparent !important;
 }
 
 .preview >>> ul {
-  list-style-type: disc; /* 默认值，圆点 */
+    list-style-type: disc; /* 默认值，圆点 */
 }
 
 .preview >>> ol {
-  list-style-type: decimal; /* 默认值，数字 */
+    list-style-type: decimal; /* 默认值，数字 */
 }
 </style>
