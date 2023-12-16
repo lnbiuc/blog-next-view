8ce9e3665abe972cb9cc0ac950e46a9d2947c1b8
lnbiuc <hi@lnbiuc.com>
2023-12-17 02:53:07 +0800

    Update routeRules in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,8 +35,8 @@ export default defineNuxtConfig({
   },
 
   routeRules: {
-    '/api/**': { proxy: { to: process.server ? 'http://blog-next-api:6060/api/**' : 'https://blog-api.vio.vin/api/**' } },
-    // '/api/**': { proxy: { to: 'https://blog-api.vio.vin/api/**' } },
+    // '/api/**': { proxy: { to: process.server ? 'http://blog-next-api:6060/api/**' : 'https://blog-api.vio.vin/api/**' } },
+    '/api/**': { proxy: { to: 'http://blog-next-api:6060/api/**' } },
   },
 
   experimental: {
