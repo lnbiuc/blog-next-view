e4f29ca6f7c6e5b95dfcaebebb45fdbea734772e
lnbiuc <hi@lnbiuc.com>
2023-12-17 03:02:25 +0800

    Update API proxy URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
 
   routeRules: {
     // '/api/**': { proxy: { to: process.server ? 'http://blog-next-api:6060/api/**' : 'https://blog-api.vio.vin/api/**' } },
-    '/api/**': { proxy: { to: 'http://blog-next-api:6060/api/**' } },
+    '/api/**': { proxy: { to: 'http://47.109.97.85:6060/api/**' } },
   },
 
   experimental: {
