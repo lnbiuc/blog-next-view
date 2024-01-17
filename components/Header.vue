a3bc876e67022bace5fef76744faacd51f3f909c
ayanamirei <violetzzs@proton.me>
2024-01-18 00:19:38 +0800

    og image gen

--- components/Header.vue
+++ components/Header.vue
@@ -14,7 +14,7 @@ function isExactRoute(currRoute: string) {
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center px-5">
+  <div id="header" class="flex flex-col items-center justify-center px-5">
     <header ref="headerRef" class="Header w-full overflow-hidden xl:m-2 lg:w-[80%] md:w-full sm:w-full xl:max-w-[1100px] xl:w-[80%] xl:rounded-md" :style="styles">
       <div class="wave-animation h-[10px] w-full" />
       <div class="w-full flex flex-row items-center justify-center">
