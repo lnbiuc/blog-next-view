30bbfea8d11b64127ed64676b7722b27c09a0574
ayanamirei <violetzzs@proton.me>
2024-01-20 01:42:40 +0800

    change api setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,7 +40,8 @@ export default defineNuxtConfig({
   routeRules: {
     // '/api/**': { proxy: { to: process ? 'http://47.109.97.85:6060/api/**' : 'https://blog-api.vio.vin/api/**' } },
     // '/api/**': { proxy: { to: 'http://47.109.97.85:6060/api/**' } },
-    '/api/**': { proxy: { to: 'https://blog-api.vio.vin/api/**' } },
+    // '/api/**': { proxy: { to: 'https://blog-api.vio.vin/api/**' } },
+    '/api/**': { proxy: { to: 'http://127.0.0.1:6060/api/**' } },
   },
 
   devProxy: {
