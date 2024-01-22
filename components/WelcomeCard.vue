77af7cebc30807ba936d57fc29601055b02ccfe7
lnbiuc <hi@lnbiuc.com>
2024-01-22 14:01:19 +0800

    add image hover effect

--- components/WelcomeCard.vue
+++ components/WelcomeCard.vue
@@ -36,6 +36,64 @@ function handleScroll() {
     window.scrollTo({ top: offsetPosition, behavior: 'smooth' })
   }
 }
+
+onMounted(() => {
+  // const favicon: HTMLElement | null = document.querySelector('.favicon-shadown')
+  // const yRotate = [-20, 20]
+  // const xRotate = [-20, 20]
+  // function getRotateDeg(range: number[], value: number, length: number) {
+  //   return (value / length) * (range[1] - range[0]) + range[0]
+  // }
+
+  // if (!favicon)
+  //   return
+  // favicon.onmousemove = (e: any) => {
+  //   const { offsetX, offsetY } = e
+  //   const { offsetWidth, offsetHeight } = favicon
+  //   const ry = -getRotateDeg(yRotate, offsetX, offsetWidth)
+  //   const rx = getRotateDeg(xRotate, offsetY, offsetHeight)
+  //   favicon.style.setProperty('--rx', `${rx}deg`)
+  //   favicon.style.setProperty('--ry', `${ry}deg`)
+  // }
+
+  // favicon.onmouseleave = () => {
+  //   favicon.style.setProperty('--rx', `0deg`)
+  //   favicon.style.setProperty('--ry', `0deg`)
+  // }
+  const card: HTMLElement | null = document.querySelector('.favicon-shadown')
+  const motionMatchMedia = window.matchMedia('(prefers-reduced-motion)')
+  const THRESHOLD = 20
+  /*
+ * Read the blog post here:
+ * https://letsbuildui.dev/articles/a-3d-hover-effect-using-css-transforms
+ */
+  function handleHover(e: any) {
+    if (!card)
+      return
+    const { clientX, clientY, currentTarget } = e
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget
+
+    const horizontal = (clientX - offsetLeft) / clientWidth
+    const vertical = (clientY - offsetTop) / clientHeight
+    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2)
+    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2)
+
+    card.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`
+  }
+
+  function resetStyles(e: any) {
+    if (!card)
+      return
+    card.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`
+  }
+
+  if (!motionMatchMedia.matches) {
+    if (!card)
+      return
+    card.addEventListener('mousemove', handleHover)
+    card.addEventListener('mouseleave', resetStyles)
+  }
+})
 </script>
 
 <template>
@@ -70,8 +128,8 @@ function handleScroll() {
         </a>
       </div>
     </div>
-    <div class="hidden transition-transform duration-150 md:block hover:scale-105">
-      <img v-auto-animate src="/favicon.ico" alt="logo" class="favicon-shadown h-[300px] w-[300px] rounded-full">
+    <div class="hidden transition-all md:block">
+      <img v-auto-animate src="/favicon.ico" alt="logo" class="favicon-shadown h-[300px] w-[300px] rounded-full transition-all">
     </div>
   </div>
   <div class="h-[100px] w-full flex flex-row justify-center">
@@ -85,12 +143,29 @@ function handleScroll() {
   src: url('/Spacewar.ttf') format('truetype');
 }
 
+.favicon-shadown:hover {
+  box-shadow: 0 0 150px 60px #8990b5;
+  transform: translateZ(12px);
+  transition: all 0.1s ease;
+}
+
+.dark .favicon-shadown:hover {
+  box-shadow: 0 0 150px 60px #335062;
+  transform: translateZ(12px);
+  transition: all 0.1s ease;
+}
+
 .favicon-shadown {
-  box-shadow: 0 0 150px 50px #8990b5;
+  box-shadow: 0 0 150px 30px #8990b5;
+  transform: rotateX(var(--rx, 0deg)) rotateY(var(--ry, 0deg));
+  transition: all 0.1s ease;
+  transform-style: preserve-3d;
 }
 
 .dark .favicon-shadown {
-  box-shadow: 0 0 150px 50px #335062;
+  box-shadow: 0 0 150px 30px #335062;
+  transition: all 0.1s ease;
+  transform-style: preserve-3d;
 }
 
 @keyframes jump {
