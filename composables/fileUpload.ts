328635514c21f4c60e56a9d727b96dc45c7c4015
ayanamirei <violetzzs@proton.me>
2024-02-15 01:41:24 +0800

    fix ts error

--- composables/fileUpload.ts
+++ composables/fileUpload.ts
@@ -18,8 +18,8 @@ export function upload(filePath: string, filename: string): string {
   const key = `/blog/${year}/${month}/${filename}`
 
   cos.putObject({
-    Bucket: process.env.BUCKET_NAME, /* 填入您自己的存储桶，必须字段 */
-    Region: process.env.REGION_NAME, /* 存储桶所在地域，例如 ap-beijing，必须字段 */
+    Bucket: process.env.BUCKET_NAME ? process.env.BUCKET_NAME : '', /* 填入您自己的存储桶，必须字段 */
+    Region: process.env.REGION_NAME ? process.env.REGION_NAME : '', /* 存储桶所在地域，例如 ap-beijing，必须字段 */
     Key: key, /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
     StorageClass: 'STANDARD',
     /* 当 Body 为 stream 类型时，ContentLength 必传，否则 onProgress 不能返回正确的进度信息 */
