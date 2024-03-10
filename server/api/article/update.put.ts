d1359e87a43d8b70f9c2dedf8ee36c1228090d96
ayanamirei <violetzzs@proton.me>
2024-03-11 00:19:03 +0800

    fix cache not update

--- server/api/article/update.put.ts
+++ server/api/article/update.put.ts
@@ -1,9 +1,11 @@
 import { ArticleSchema } from '~/server/models/article.schema';
+import { cache } from '~/config/cache.config';
 
 export default defineEventHandler(async event => {
 	try {
 		const body = await readBody(event);
 		if (!body._id || body._id === '') return { error: 'missing _id' };
+		cache.del(body.shortLink);
 		return await ArticleSchema.findByIdAndUpdate(body._id, body, { new: true });
 	} catch (error) {
 		return new Response(error as string, { status: 500 });
