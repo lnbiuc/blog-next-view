0a59e686479e2dbda0b0abccbeadb1ea1265c0ec
ayanamirei <violetzzs@proton.me>
2024-04-08 12:19:07 +0800

    add anim

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -106,7 +106,7 @@ export default defineNuxtConfig({
 	},
 
 	app: {
-		// pageTransition: { name: 'page', mode: 'out-in' },
+		pageTransition: { name: 'page', mode: 'out-in' },
 		head: {
 			viewport: 'width=device-width,initial-scale=1',
 			link: [
