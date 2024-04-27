307b5feff8644e23633b62c6f28749a4555488ac
violet <hi@lnbiuc.copm>
2024-04-27 10:19:58 +0800

    lint

--- server/api/user/index.get.ts
+++ server/api/user/index.get.ts
@@ -1,6 +1,6 @@
 import { UserSchema } from '~/server/models/user.schema'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
   try {
     return await UserSchema.find({})
   }
